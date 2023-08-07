2c741bb746cc536314c0e182a7b3b227141bc7a2
Aimen Ellahi <you@example.com>
2023-08-07 18:36:51 +0500

    added form

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import useMenuStore from "../Utils/menuStore"; 
+import { Select, MenuItem} from "@mui/material";
+
 
 const options = {
     "Select your Hull (single select)": [
@@ -55,6 +57,8 @@ const options = {
   
   
   const Menu = () => {
+   
+    // const [isModalOpen, setModalOpen] = useState(false);
     const totalPrice = useMenuStore((state) => state.totalPrice);
     const selectedOptions = useMenuStore((state) => state.selectedOptions);
     const updateTotalPrice = useMenuStore((state) => state.updateTotalPrice);
@@ -107,188 +111,210 @@ const options = {
         }));
     };
 
+    // const handleSubmit = () => {
+    //   handleModalOpen();
+    // };
+
+    // const handleModalOpen = () => {
+    //   setModalOpen(true);
+    // };
     
-      const handleSubmit = () => {
-        // Here you can implement the logic to submit the user's selections and form data.
-        // You can access the selectedOptions and userForm state variables here and send them to your server or perform any other actions.
-        console.log("Selected Options:", selectedOptions);
-        console.log("User Form Data:", userForm);
-      };
+    // const handleModalClose = () => {
+    //   setModalOpen(false);
+    // };
+      
   
     return (
       <div className="menu-container">
         <div className="centered">
-          <div className="menu-header">Total Price: ${totalPrice.toFixed(2)}</div>
+          <div className="menu-header">Total: ${totalPrice.toFixed(2)}</div>
           <div className="menu-header-feature">Select Feature</div>
         </div>
-
-        <div className="menu-items-container">
+        
+      <div className="menu-items-container">
         <div className="menu-items">
           {Object.entries(options).map(([heading, items], index) => (
             <div key={index}>
               <div className="menu-heading">{heading}</div>
-              {items.map((item, idx) => (
-                <div key={idx} className="menu-item">
-                  {heading.includes("(single select)") ? (
-                    <>
-                      <input
-                        type="checkbox"
-                        checked={singleSelectGroups[heading] === item}
-                        onChange={() =>
-                          handleSingleSelectChange(heading, item)
-                        }
-                      />
-                      <label>{item.name} - Price: ${item.price.toFixed(2)}</label>
-                    </>
-                  ) : (
-                    <>
+              {heading.includes("(single select)") ? (
+                  <Select
+                  value={singleSelectGroups[heading]?.name || ""}
+                  onChange={(e) => handleSingleSelectChange(heading, e.target.value)}
+                  sx={{ height: 40 ,minWidth: 320, mb: 2, }}
+                  MenuProps={{
+                    PaperProps: {
+                      style: {
+                        width: "200px", // Set the desired width for the pop-up menu
+                      },
+                    },
+                  }}
+                >
+                  <MenuItem value="" sx={{
+                    fontSize: 14,
+                  }}>
+                    <em>Select an option</em>
+                  </MenuItem>
+                  {items.map((item, idx) => (
+                    <MenuItem key={idx} value={item} sx={{
+                      fontSize: 12,
+                    }}>
+                      {item.name} - Price: ${item.price.toFixed(2)}
+                    </MenuItem>
+                  ))}
+                </Select>
+              ) : (
+                <>
+                  {items.map((item, idx) => (
+                    <div key={idx} className="menu-item">
                       <input
                         type="checkbox"
                         checked={selectedOptions.includes(item)}
-                        onChange={(e) =>
-                          handleMultiSelectChange(item, e.target.checked)
-                        }
+                        onChange={(e) => handleMultiSelectChange(item, e.target.checked)}
                       />
                       <label>{item.name} - Price: ${item.price.toFixed(2)}</label>
                       {item.name === "6’ Power pole - $1,950" && (
                         <div className="quantity-input">
-                        <label>Quantity:</label>
-                        <input
-                          type="number"
-                          min="0"
-                          max="2"
-                          value={selectedOptions.filter(
-                            (option) => option.name === item.name
-                          ).length}
-                          onChange={(e) =>
-                            handleQuantityChange(item, parseInt(e.target.value))
-                          }
-                        />
-                      </div>
+                          <label>Quantity:</label>
+                          <input
+                            type="number"
+                            min="0"
+                            max="2"
+                            value={selectedOptions.filter((option) => option.name === item.name).length}
+                            onChange={(e) =>
+                              handleQuantityChange(item, parseInt(e.target.value))
+                            }
+                          />
+                        </div>
                       )}
-                    </>
-                  )}
-                </div>
-              ))}
+                    </div>
+                  ))}
+                </>
+              )}
             </div>
           ))}
         </div>
         
         <hr />
-      <div className="user-form">
-        <h2 className="user-form-heading">User Form</h2>
-        <div className="form-field">
-          <label htmlFor="firstName">First Name*</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={userForm.firstName}
-            onChange={handleUserFormChange}
-            required
-          />
+        <div className="centered">
+          
+          <div className="menu-header-info">User Information</div>
         </div>
-        <div className="form-field">
-          <label htmlFor="lastName">Last Name*</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={userForm.lastName}
-            onChange={handleUserFormChange}
-            required
-          />
+        <div className="user-form">
+    <div className="form-field">
+      <label htmlFor="firstName">First Name*</label>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        value={userForm.firstName}
+        onChange={handleUserFormChange}
+        required
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="lastName">Last Name*</label>
+      <input
+        type="text"
+        id="lastName"
+        name="lastName"
+        value={userForm.lastName}
+        onChange={handleUserFormChange}
+        required
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="phone">Phone*</label>
+      <input
+        type="tel"
+        id="phone"
+        name="phone"
+        value={userForm.phone}
+        onChange={handleUserFormChange}
+        required
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="email">Email*</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={userForm.email}
+        onChange={handleUserFormChange}
+        required
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="address">Address*</label>
+      <input
+        type="text"
+        id="address"
+        name="address"
+        value={userForm.address}
+        onChange={handleUserFormChange}
+        required
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="streetAddress">Street Address</label>
+      <input
+        type="text"
+        id="streetAddress"
+        name="streetAddress"
+        value={userForm.streetAddress}
+        onChange={handleUserFormChange}
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="city">City</label>
+      <input
+        type="text"
+        id="city"
+        name="city"
+        value={userForm.city}
+        onChange={handleUserFormChange}
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="state">State</label>
+      <input
+        type="text"
+        id="state"
+        name="state"
+        value={userForm.state}
+        onChange={handleUserFormChange}
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="postalZip">Postal Zip</label>
+      <input
+        type="text"
+        id="postalZip"
+        name="postalZip"
+        value={userForm.postalZip}
+        onChange={handleUserFormChange}
+      />
+    </div>
+    <div className="form-field">
+      <label htmlFor="country">Country</label>
+      <select
+        id="country"
+        name="country"
+        value={userForm.country}
+        onChange={handleUserFormChange}
+      >
+        <option value="">Select a country</option>
+        <option value="USA">USA</option>
+        <option value="Canada">Canada</option>
+        <option value="Mexico">Mexico</option>
+        <option value="Other">Other</option>
+      </select>
+    </div>
+    
+  </div>
+        <div className="centered">
+        <button className="submit-button" >Submit</button>
         </div>
-        <div className="form-field">
-          <label htmlFor="phone">Phone*</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            value={userForm.phone}
-            onChange={handleUserFormChange}
-            required
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="email">Email*</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={userForm.email}
-            onChange={handleUserFormChange}
-            required
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="address">Address*</label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            value={userForm.address}
-            onChange={handleUserFormChange}
-            required
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="streetAddress">Street Address</label>
-          <input
-            type="text"
-            id="streetAddress"
-            name="streetAddress"
-            value={userForm.streetAddress}
-            onChange={handleUserFormChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="city">City</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            value={userForm.city}
-            onChange={handleUserFormChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="state">State</label>
-          <input
-            type="text"
-            id="state"
-            name="state"
-            value={userForm.state}
-            onChange={handleUserFormChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="postalZip">Postal Zip</label>
-          <input
-            type="text"
-            id="postalZip"
-            name="postalZip"
-            value={userForm.postalZip}
-            onChange={handleUserFormChange}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="country">Country</label>
-          <select
-            id="country"
-            name="country"
-            value={userForm.country}
-            onChange={handleUserFormChange}
-          >
-            <option value="">Select a country</option>
-            <option value="USA">USA</option>
-            <option value="Canada">Canada</option>
-            <option value="Mexico">Mexico</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <button className="submit-button" onClick={handleSubmit}>Submit</button>
-      </div>
         </div>
 
       </div>
